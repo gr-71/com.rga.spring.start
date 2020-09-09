1b44c354772a44df8e1d1625b1cab484f182db9a
grybentsov <grybentsov@gmail.com>
2020-09-09 20:57:41 +0300

    Homework-to-lesson2

---
+++ homework2/src/main/java/com/rga/homework2/repository/ProductRepoImpl.java
@@ -0,0 +1,55 @@
+package com.rga.homework2.repository;
+
+import org.springframework.stereotype.Repository;
+import com.rga.homework2.domain.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ProductRepoImpl {
+
+    private Map<Long, Product> repo = new HashMap<>();
+    private long ind = 0;
+
+    {
+        repo.put(++ind, new Product(ind, "Cheese", 635.5));
+        repo.put(++ind, new Product(ind, "Milk", 66.0));
+        repo.put(++ind, new Product(ind, "Chocolate", 96.0));
+        repo.put(++ind, new Product(ind, "Bread", 39.0));
+        repo.put(++ind, new Product(ind, "Beer", 88.0));
+    }
+
+    public Product getById(Long id){
+        return repo.get(id);
+    }
+
+    public List<Product> getAll(){
+        return new ArrayList<>(repo.values());
+    }
+
+    public Product save(Product product){
+        if(product.getId() == null){
+            product.setId(++ind);
+        }
+        repo.put(product.getId(), product);
+        return product;
+    }
+
+// Добавил удаление продукта по ID :
+    public void deleteById (Long id){
+        for (int i = 0; i < repo.size(); i++) {
+            if (repo.get(i).getId().equals(id)){
+                repo.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void remove(Long id){
+        repo.remove(id);
+    }
+
+}
---
+++ homework2/src/main/java/com/rga/homework2/service/ProductServiceImpl.java
@@ -0,0 +1,52 @@
+package com.rga.homework2.service;
+
+import org.springframework.stereotype.Service;
+import com.rga.homework2.domain.Product;
+import com.rga.homework2.repository.ProductRepoImpl;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductServiceImpl {
+
+    private ProductRepoImpl productRepo;
+
+    public ProductServiceImpl(ProductRepoImpl productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public Product getById(Long id){
+        return productRepo.getById(id);
+    }
+
+    public List<Product> getAll(){
+        List<Product> products = productRepo.getAll();
+        products.sort(Comparator.comparingLong(Product::getId));
+        return products;
+    }
+
+    public List<Product> getByPrice(Double start, Double end){
+        return productRepo.getAll().stream()
+                .filter(product-> product.getPrice() >= start && product.getPrice() <= end)
+                .sorted(Comparator.comparingDouble(Product::getPrice))
+                .collect(Collectors.toList());
+    }
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    // Добавил метод удаления продукта по ID
+    public void deleteProductById (Long id){
+        productRepo.deleteById(id);
+    }
+
+    public void removeById(Long id){
+        productRepo.remove(id);
+    }
+
+
+
+}
